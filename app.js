dca2324de951119cb42b6c160d029f6caa8d7ee6
12three <zakharenko.main@gmail.com>
2019-08-12 14:24:54 +0300

    set up temlating engine

--- app.js
+++ app.js
@@ -2,32 +2,27 @@
 var express = require('express');
 var path = require('path');
 var log = require('./libs/log')(module);
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-// middlewares
-app.use((req, res) => {
-  res.end('hello');
-  log.info('logged')
-})
-
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs-locals'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine' , 'ejs');
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
