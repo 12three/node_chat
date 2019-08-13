445c529b8a471e7a7576a0b045762eb44a0f555f
12three <zakharenko.main@gmail.com>
2019-08-13 18:35:26 +0300

    add ability to work with session

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-// const createError = require('http-errors');
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const log = require('./libs/log')(module);
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');)
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const session = require('express-session');
+const config = require('./config');
+const logger = require('morgan');
+const mongoose = require('./libs/mongoose');
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 
@@ -19,25 +20,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+    session({
+        secret: config.get('session:secret'),
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    }),
+);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+require('./routes')(app);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const router = function(app) {
+    app.get('/', function(req, res, next) {
+        res.render('index', { title: 'Express' });
+    });
+};
 
 module.exports = router;
